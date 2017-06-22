6eafd235af8545c37e59c1a6bf5a8af653170f12
jimmykimani <james.kimani@andela.com>
2017-06-22 14:54:54 +0300

    (Fix) Pep8 errors

--- wger/exercises/sitemap.py
+++ wger/exercises/sitemap.py
@@ -25,5 +25,7 @@ class ExercisesSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        language_list = load_item_languages(LanguageConfig.SHOW_ITEM_EXERCISES)
-        return Exercise.objects.accepted().filter(language__in=language_list)
+        language_list = load_item_languages
+        (LanguageConfig.SHOW_ITEM_EXERCISES)
+        return Exercise.objects.accepted().
+        filter(language__in=language_list)
