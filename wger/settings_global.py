d2e21fe466dd61223e1479b990a763e9d93f9f0c
Eric G <eric.gichuri@andela.com>
2017-07-11 15:26:39 +0300

    1st Release - Merge completed tasks (#18)
    * add slack notification key
    
    * correct indentation to make travis pass
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * (Feature) Add heroku procfile
    
    * Add changes to requiremennts file
    
    * Add gunicorn file
    
    * (Bug) Fic errors
    
    * (Feature) Add whitenoise
    
    * (Feature) Add Dj database config
    
    * (Feature) Add settings file
    
    * (Feature) Add runtime file for build
    
    * Fixes style errors across the codebase  #147322149 (#5)
    
    * [Fix 147322149] Fix failing tests, lengthen passoword
    
    * [Chore 147322149] Fix styling errors , improved formatting
    
    used yapf, autopep8 to fix styling errors
    Improved code readability
    
    * [Chore 147322149] Add configuration file
    
    * (Fix) Static root files
    
    * (Fix) Travis error build
    
    * Update settings_global.py
    
    Correct indentation
    
    * Update requirements.txt
    
    * Improves Travis Efficiency by trimming some Builds  #148105193 (#9)
    
    * [Chore 148105193] Fix double requirement given error
    
    * [Chore 148105193] Improve travis efficiency
    
    * Removes unwanted python versions
    * Removes sqlite from builds
    
    * [Chore 148105193] Remove custom database settings
    
    * [Chore 148105193] Use main requirements file
    
    * [Chore 148105193] Add missing covergae package
    
    * [Chore 148105193] Clean up branch
    
    * Ft enable social login #147322191 (#10)
    
    * [chore #147322191] update global settings
    
    * [chore #147322191] update .gitignore
    
    * [feature #147322191] Add social auth app url
    
    * [chore #147322191] refactor settings
    
    * [feature #147322191] Add social login buttons
    
    * [Feature #14732219] Add login buttons to login page
    
    * [Fix  #14732219] Remove login buttons from landing page
    
    * [Chore #147322191] Update migrations
    
    * [feature #147322191] Add urls for social logins
    
    * [Feature #147322191] Change credentials for twitter login
    
    * [Chore #147322191] Change url for google login
    
    * [Chore #147322191] Update social logi crdentials
    
    * [Chore #147322191] Refactor social login
    
    * [Chore  #147322191] Update requirements file
    
    * [Chore  #147322191] Update requirements file
    
    * [Fix  #147322191] Remove double requirements in requirements file
    
    * [Fix  #147322191] Remove double requirements in requirements file
    
    * [Chore #147322191] Rework login url
    
    * [chore] Refine requirements
    
    * [Feature #147322191] Add social login on landing page, and registration page (#17)

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -19,6 +19,8 @@
 import os
 import re
 import sys
+import dj_database_url
+
 '''
 This file contains the global settings that don't usually need to be changed.
 For a full list of options, visit:
@@ -27,6 +29,8 @@ For a full list of options, visit:
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+DATABASES = {'default': dj_database_url.config()}
 
 #
 # Application definition
@@ -45,6 +49,9 @@ INSTALLED_APPS = (
 
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+
+    # app for social login
+    'social_django',
 
     # Apps from wger proper
     'wger.config',
@@ -102,7 +109,8 @@ MIDDLEWARE_CLASSES = (
     # Javascript Header. Sends helper headers for AJAX
     'wger.utils.middleware.JavascriptAJAXRedirectionMiddleware',
 
-    # Custom authentication middleware. Creates users on-the-fly for certain paths
+    # Custom authentication middleware. Creates users on-the-fly for
+    # certain paths
     'wger.utils.middleware.WgerAuthenticationMiddleware',
 
     # Send an appropriate Header so search engines don't index pages
@@ -113,11 +121,21 @@ MIDDLEWARE_CLASSES = (
 
     # Django mobile
     'django_mobile.middleware.MobileDetectionMiddleware',
-    'django_mobile.middleware.SetFlavourMiddleware', )
+    'django_mobile.middleware.SetFlavourMiddleware',
 
-AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
-                           'wger.utils.helpers.EmailAuthBackend')
+    # social login auth
+    'social_django.middleware.SocialAuthExceptionMiddleware'
+)
 
+AUTHENTICATION_BACKENDS = (
+    'wger.utils.helpers.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend'
+
+)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -134,6 +152,8 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
 
                 # Django mobile
                 'django_mobile.context_processors.flavour',
@@ -239,7 +259,6 @@ LOGGING = {
     }
 }
 
-#
 # ReCaptcha
 #
 RECAPTCHA_USE_SSL = True
@@ -260,43 +279,20 @@ CACHES = {
 #
 THUMBNAIL_ALIASES = {
     '': {
-        'micro': {
-            'size': (30, 30)
-        },
-        'micro_cropped': {
-            'size': (30, 30),
-            'crop': 'smart'
-        },
-        'thumbnail': {
-            'size': (80, 80)
-        },
-        'thumbnail_cropped': {
-            'size': (80, 80),
-            'crop': 'smart'
-        },
-        'small': {
-            'size': (200, 200)
-        },
-        'small_cropped': {
-            'size': (200, 200),
-            'crop': 'smart'
-        },
-        'medium': {
-            'size': (400, 400)
-        },
-        'medium_cropped': {
-            'size': (400, 400),
-            'crop': 'smart'
-        },
-        'large': {
-            'size': (800, 800),
-            'quality': 90
-        },
-        'large_cropped': {
-            'size': (800, 800),
-            'crop': 'smart',
-            'quality': 90
-        },
+        'micro': {'size': (30, 30)},
+        'micro_cropped': {'size': (30, 30), 'crop': 'smart'},
+
+        'thumbnail': {'size': (80, 80)},
+        'thumbnail_cropped': {'size': (80, 80), 'crop': 'smart'},
+
+        'small': {'size': (200, 200)},
+        'small_cropped': {'size': (200, 200), 'crop': 'smart'},
+
+        'medium': {'size': (400, 400)},
+        'medium_cropped': {'size': (400, 400), 'crop': 'smart'},
+
+        'large': {'size': (800, 800), 'quality': 90},
+        'large_cropped': {'size': (800, 800), 'crop': 'smart', 'quality': 90},
     },
 }
 
@@ -305,11 +301,14 @@ THUMBNAIL_ALIASES = {
 #
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # The default is not DEBUG, override if needed
 # COMPRESS_ENABLED = True
-COMPRESS_CSS_FILTERS = ('compressor.filters.css_default.CssAbsoluteFilter',
-                        'compressor.filters.cssmin.rCSSMinFilter')
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.rCSSMinFilter'
+)
 COMPRESS_ROOT = STATIC_ROOT
 
 # BOWER binary
@@ -324,18 +323,16 @@ else:
 # Django Rest Framework
 #
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('wger.utils.permissions.WgerPermission', ),
-    'PAGINATE_BY':
-    20,
-    'PAGINATE_BY_PARAM':
-    'limit',  # Allow client to override, using `?limit=xxx`.
-    'TEST_REQUEST_DEFAULT_FORMAT':
-    'json',
-    'DEFAULT_AUTHENTICATION_CLASSES':
-    ('rest_framework.authentication.SessionAuthentication',
-     'rest_framework.authentication.TokenAuthentication', ),
+    'DEFAULT_PERMISSION_CLASSES': ('wger.utils.permissions.WgerPermission',),
+    'PAGINATE_BY': 20,
+    'PAGINATE_BY_PARAM': 'limit',  # Allow client to override, using `?limit=xxx`.
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',
-                                'rest_framework.filters.OrderingFilter', )
+                                'rest_framework.filters.OrderingFilter',)
 }
 
 #
@@ -347,7 +344,10 @@ CORS_URLS_REGEX = r'^/api/.*$'
 #
 # Ignore these URLs if they cause 404
 #
-IGNORABLE_404_URLS = (re.compile(r'^/favicon\.ico$'), )
+
+IGNORABLE_404_URLS = (
+    re.compile(r'^/favicon\.ico$'),
+)
 
 #
 # Password rules
@@ -384,3 +384,18 @@ WGER_SETTINGS = {
     'EMAIL_FROM': 'wger Workout Manager <wger@example.com>',
     'TWITTER': False
 }
+
+# facebook, twitter and google API keys
+# configurations for twitter
+SOCIAL_AUTH_TWITTER_KEY = os.getenv("TWITTER_KEY")
+SOCIAL_AUTH_TWITTER_SECRET = os.getenv("TWITTER_SECRET")
+
+# configurations for facebook
+SOCIAL_AUTH_FACEBOOK_SECRET = os.getenv("FB_SECRET")
+SOCIAL_AUTH_FACEBOOK_KEY = os.getenv("FB_KEY")
+
+# configurations for google
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_SECRET")
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_KEY")
+
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
