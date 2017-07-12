be4c3b62d145288b71e637cc5e6c9c43e60b0d57
Dominic Motuka <dominic.motuka@gmail.com>
2017-07-12 11:55:59 +0300

    [Feature 147322155] Add nutrition plan cache key

--- wger/utils/cache.py
+++ wger/utils/cache.py
@@ -66,6 +66,7 @@ class CacheKeyMapper(object):
     INGREDIENT_CACHE_KEY = 'ingredient-{0}'
     WORKOUT_CANONICAL_REPRESENTATION = 'workout-canonical-representation-{0}'
     WORKOUT_LOG_LIST = 'workout-log-hash-{0}'
+    NUTRITION_PLAN_CACHE_KEY = 'nutrition-plan-{0}'
 
     def get_pk(self, param):
         '''
@@ -113,6 +114,12 @@ class CacheKeyMapper(object):
         Return the workout canonical representation
         '''
         return self.WORKOUT_LOG_LIST.format(hash_value)
+
+    def get_nutrition_plan(self, param):
+        '''
+        Return the nutrition plan key
+        '''
+        return self.NUTRITION_PLAN_CACHE_KEY.format(self.get_pk(param))
 
 
 cache_mapper = CacheKeyMapper()
